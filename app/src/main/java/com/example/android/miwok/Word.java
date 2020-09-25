e3e6b92b144cab30ae38fda960fccdc52f51164f
nishit1606 <nishit1606@gmail.com>
2020-09-25 15:52:04 +0530

    Added Visibility for Image

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -8,7 +8,11 @@ public class Word {
 
     private String mMiwokTranslation;
 
-    private int mImageResourceId;
+
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+
+    private static final int NO_IMAGE_PROVIDED = -1;
 
 
 
@@ -17,23 +21,28 @@ public class Word {
         mMiwokTranslation = miwokTranslation;
     }
 
-    public Word(String defaultTranslation, String miwokTranslation,int ImageResourceId) {
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
-        mImageResourceId=ImageResourceId;
+        mImageResourceId = imageResourceId;
     }
-
 
     public String getDefaultTranslation() {
         return mDefaultTranslation;
     }
 
-
     public String getMiwokTranslation() {
         return mMiwokTranslation;
     }
 
-    public int getmImageResourceId(){
+
+    public int getImageResourceId() {
         return mImageResourceId;
+    }
+
+
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
     }
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -39,6 +40,17 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         defaultTextView.setText(currentWord.getDefaultTranslation());
 
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+        // Check if an image is provided for this word or not
+        if (currentWord.hasImage()) {
+            // If an image is available, display the provided image based on the resource ID
+            imageView.setImageResource(currentWord.getImageResourceId());
+            // Make sure the view is visible
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            // Otherwise hide the ImageView (set visibility to GONE)
+            imageView.setVisibility(View.GONE);
+        }
 
         return listItemView;
     }
